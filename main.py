d32154012b9bb8f2aca07e5bd7e05adf5902674f
dima vinnichuk <1dima.vinnichuk@gmail.com>
2026-02-02 14:01:58 +0100

    CICD test

--- main.py
+++ main.py
@@ -8,4 +8,8 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
-    return {"item_id": item_id, "processed": True}
+    return {
+        "item_id": item_id, 
+        "processed": True,
+        "version": "1.0.1"
+    }
